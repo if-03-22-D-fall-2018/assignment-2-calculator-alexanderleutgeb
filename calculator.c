ee085eb06b78854a4c782e3ccf2d19eabd019ff6
alexanderleutgeb <alex.leutgeb02@gmail.com>
2018-09-27 20:35:47 +0200

    basics changed

--- calculator.c
+++ calculator.c
@@ -1,6 +1,45 @@
 #include <stdio.h>
 
-int main(int argc, char const *argv[]) {
+void AddOperands(){
+  GetFirstOperand();
+  GetSecondOperand();
+  int result;
+  result = firstOperand+secondOperand
+  printf("Result: %lf\n", result);
+}
+void SubOperands(){
+  GetFirstOperand();
+  GetSecondOperand();
+  int result;
+  result = firstOperand-secondOperand
+  printf("Result: %lf\n", result);
+}
+void MultiplyOperands(){
+  GetFirstOperand();
+  GetSecondOperand();
+  int result;
+  result = firstOperand*secondOperand
+  printf("Result: %lf\n", result);
+}
+void DivideOperands(){
+  GetFirstOperand();
+  GetSecondOperand();
+  int result;
+  result = firstOperand/secondOperand
+  printf("Result: %lf\n", result);
+}
+int GetFirstOperand(){
+  double firstOperand;
+
+  return firstOperand;
+}
+int GetSecondOperand(){
+  double secondOperand;
+  
+  return secondOperand;
+}
+int main(int argc, char const *argv[])
+{
   printf("Choose one of the following operations: ");
   printf("Add(1)\n");
   printf("Subtract(2)\n");
@@ -10,59 +49,20 @@ int main(int argc, char const *argv[]) {
   printf("Enter your Choice:\n");
 
   int type_of_operation;
+  printf("Enter yout choice: ");
   scanf("%d\n", &type_of_operation);
 
-  if (type_of_operation == -1) {
-    printf("Programm quit by user request\n");
+  switch (type_of_operation) {
+    case -1:
+      printf("Programm closes by users desission\n");
+      return 1;
+    case 1:
+      AddOperands();
+    case 2:
+      SubOperands();
+    case 3:
+      MultiplyOperands();
+    case 4:
+      DivideOperands();
   }
-
-  else if (type_of_operation == 1) {
-    Get_first_number();
-    Get_second_number();
-    double result = first_number+second_number;
-    printf("Result: %lf\n", result);
-  }
-  else if (type_of_operation == 2) {
-    Get_first_number();
-    Get_second_number();
-    double result = first_number-second_number;
-    printf("Result: %lf\n", result);
-  }
-  else if (type_of_operation == 3) {
-    Get_first_number();
-    Get_second_number();
-    double result = first_number*second_number;
-    printf("Result: %lf\n", result);
-  }
-  else if (type_of_operation == 4) {
-    Get_first_number();
-    Get_second_number();
-    if (second_number == 0) {
-      printf("division by zero\n", );
-    }
-    else{
-      result = first_number/second_number;
-      printf("Result: %ld\n", &result);
-    }
-    double result = first_number/second_number;
-    printf("Result: %lf\n", result);
-  }
-  else{
-    printf("Invalid Input\n");
-  }
-  return 0;
-}
-//Gets the first number
-double Get_first_number(){
-  printf("Please enter the first operand: \n");
-  double first_operand;
-  scanf("%lf\n",&first_operand);
-  return first_operand;
-}
-//Gets the second number
-double Get_second_number(){
-  printf("Please enter the second operand: \n");
-  double second_operand;
-  scanf("%lf\n",&second_operand);
-  return second_operand;
 }
